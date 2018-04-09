1ef330063310e9135d2e24906e1c234c2ac0dc0e
Mike Ziehfreund <mikeziehfreund@gmail.com>
2018-04-09 14:15:42 -0400

    item module try

---
+++ scripts/Item.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const globalVar = globe; 
+
+const newIife = (function () {
+        return {
+            newObj = {};
+        }
+
+    }
+
+
+);
+
+const foo = 'bar';
+
--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,6 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
+
+console.log(Item);
+console.log(foo);
